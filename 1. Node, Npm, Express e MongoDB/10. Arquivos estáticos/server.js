bd17b979f50d4bc3eaa043dbd0981257d920b268
BrunoAlencar27 <Bruno.alencar.2017@gmail.com>
2025-09-03 17:40:16 -0300

    sobe

---
+++ 1. Node, Npm, Express e MongoDB/10. Arquivos estáticos/server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const routes = require('./routes.js');
+
+
+app.use(
+    express.urlencoded(
+        {
+            extended:true
+        }
+    )
+);
+
+app.use(routes);
+app.use(express.static(path.resolve(__dirname,'public')));
+
+app.set('views', path.resolve(__dirname,'src','views'));
+app.set('view engine', 'ejs');
+
+
+app.listen(3000,()=>{
+    
+});
